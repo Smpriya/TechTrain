fba27987f84eebd6a635db8b0f81a0a3d9b82000
Shiva <isha@abcd.com>
2020-07-21 23:15:43 +0530

    angular test

--- Ang/angTest/src/app/utility.service.spec.ts
+++ Ang/angTest/src/app/utility.service.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UtilityService } from './utility.service';
+import { SecurityContext } from '@angular/core';
+import { of } from 'rxjs';
 
 describe('UtilityService', () => {
   let service: UtilityService;
@@ -13,4 +15,138 @@ describe('UtilityService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  //Start
+
+
+  describe('all', () => {
+
+
+    it('test case 1', () => {
+      const userresponse = [
+        {
+          id: "1",
+          name: "Abcd",
+          role: "developer",
+          tech: "java"
+        },
+        {
+          id: "2",
+          name: "XYZ",
+          role: "Sr developer",
+          tech: "java"
+        },
+        {
+          id: "3",
+          name: "1234",
+          role: "architect",
+          tech: "fullstack"
+        },
+        {
+          id: "4",
+          name: "asdf",
+          role: "tester",
+          tech: "selenium"
+        },
+      ];
+
+      let response;
+
+      spyOn(service, 'all').and.returnValue(of(userresponse));
+
+      service.all().subscribe(res => {
+        response = res;
+      })
+
+      expect(response).toEqual(userresponse);
+
+    });
+
+    it('test case 2..', () => {
+      const userresponse = [
+        {
+          id: "1",
+          name: "Abcd",
+          role: "developer",
+          tech: "java"
+        },
+        {
+          id: "2",
+          name: "XYZ",
+          role: "Sr developer",
+          tech: "java"
+        },
+        {
+          id: "3",
+          name: "1234",
+          role: "architect",
+          tech: "fullstack"
+        },
+        {
+          id: "4",
+          name: "asdf",
+          role: "tester",
+          tech: "selenium"
+        },
+      ];
+
+      let response;
+
+      service.all().subscribe(res => {
+        response = res;
+      })
+
+      expect(response).toEqual(userresponse);
+    });
+
+    it('test case 3', () => {
+      const userresponse = [
+        {
+          id: "2",
+          name: "XYZ",
+          role: "Sr developer",
+          tech: "java"
+        }
+      ];
+
+      let response;
+
+      service.findone("2").subscribe(res => {
+        response = res;
+      })
+      console.log(response);
+      console.log(userresponse);
+      expect(response).toEqual(userresponse[0]);
+    });
+
+    it ('all()2 test case', () => {
+      const userresponse = [
+        {
+          id: "2",
+          name: "XYZ",
+          role: "Sr developer",
+          tech: "java"
+        }
+      ];
+
+      let response;
+
+      spyOn(service, 'all2').and.returnValue(of(userresponse));
+
+      service.all2().subscribe(res => {
+        response = res;
+      })
+
+      expect(response).toEqual(userresponse);
+    });
+
+
+  });
+
+
 });
+
+  //Emd
+
+
+//});
--- Ang/angTest/src/app/utility.service.ts
+++ Ang/angTest/src/app/utility.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { of, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +9,45 @@ export class UtilityService {
   public users:Array<object> = [
     {
       id:"1",
-      name:"Abd"
-    }
-  ]
+      name:"Abcd",  
+      role:"developer",
+      tech:"java"
+    },
+    {
+      id:"2",
+      name:"XYZ",
+      role:"Sr developer",
+      tech:"java"
+    },
+    {
+      id:"3",
+      name:"1234",
+      role:"architect",
+      tech:"fullstack"
+    },
+    {
+      id:"4",
+      name:"asdf",
+      role:"tester",
+      tech:"selenium"
+    },
+  ];
 
   constructor() { }
+
+  all():Observable<Array<object>>{
+    return of(this.users);
+  }
+
+  all2():Observable<Array<object>>{
+    return of([{}]);
+  }
+
+  findone(id:string):Observable<object> {
+    const user = this.users.find((u:any) => {
+      return u.id === id;
+    });
+    return of(user)
+  }
+
 }
