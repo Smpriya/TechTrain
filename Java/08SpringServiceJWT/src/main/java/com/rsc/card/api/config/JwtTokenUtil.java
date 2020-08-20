0ebb9426cd0f7ce7bbd11d070597c9f6de59236e
Shiva <isha@abcd.com>
2020-08-20 23:43:01 +0530

    iv2

--- Java/08SpringServiceJWT/src/main/java/com/rsc/card/api/config/JwtTokenUtil.java
+++ Java/08SpringServiceJWT/src/main/java/com/rsc/card/api/config/JwtTokenUtil.java
@@ -1,7 +1,20 @@
 package com.rsc.card.api.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
 public class JwtTokenUtil implements Serializable {
 	private static final long serialVersionUID = -2550185165626007488L;
 
