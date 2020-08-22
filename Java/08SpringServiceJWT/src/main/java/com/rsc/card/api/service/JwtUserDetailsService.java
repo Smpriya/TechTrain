571d56a2f4886df1f63d43bf3ad7c44891e60391
Shiva <isha@abcd.com>
2020-08-22 23:34:24 +0530

    iv

--- Java/08SpringServiceJWT/src/main/java/com/rsc/card/api/service/JwtUserDetailsService.java
+++ Java/08SpringServiceJWT/src/main/java/com/rsc/card/api/service/JwtUserDetailsService.java
@@ -2,20 +2,31 @@ package com.rsc.card.api.service;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.rsc.card.api.repo.UserDao;
 
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
+	
+	@Autowired
+	private UserDao userDao;
+
+	@Autowired
+	private PasswordEncoder bcryptEncoder;
+	
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		if ("javainuse".equals(username)) {
+		if ("rsc".equals(username)) {
 			//rsc123
-			return new User("rsc", "$2a$10$TvcCvbnER0XC9YOZVT5TneLyUdrlIPHeYu.S4kWTZMlKUunbIZ0pS",
+			return new User("rsc", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
 					new ArrayList<>());
 		} else {
 			throw new UsernameNotFoundException("User not found with username: " + username);
